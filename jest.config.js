e7b0ccdf8121b7460abff7dd9b63cc3dd5673ee3
xoxomakkie <makaylagunterm@hotmail.com>
2025-09-09 18:16:23 -0400

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js'],
+  collectCoverageFrom: [
+    '**/*.js',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!jest.config.js',
+    '!server.js'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'html'],
+  verbose: true,
+  // Increase timeout for database operations
+  testTimeout: 10000
+};
